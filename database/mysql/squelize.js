bf7b8aff365d46ab673bf4b548a7943e2cd223f7
CrystalAngelLee <crystaljiaxuan@163.com>
2020-05-21 20:43:37 +0800

    MongoDB学习代码提交

--- database/mysql/squelize.js
+++ database/mysql/squelize.js
@@ -10,30 +10,30 @@
     });
 
     // 定义模型
-    const Fruit = sequelize.define("Fruit", {
-        name: {
-            type: Sequelize.STRING(20),
-            allowNull: false,
-            // get() {
-            //     const fname = this.getDataValue("name");
-            //     const price = this.getDataValue("price");
-            //     const stock = this.getDataValue("stock");
-            //     return `${fname}(价格：￥${price} 库存：${stock}kg)`;
-            // }
-        },
-        price: {
-            type: Sequelize.FLOAT,
-            allowNull: false,
-            validate: {
-                isFloat: { msg: "价格字段请输入数字" },
-                min: { args: [0], msg: "价格字段必须大于0" }
-            }
-        },
-        stock: { type: Sequelize.INTEGER, defaultValue: 0 },
-
-    },
+    const Fruit = sequelize.define("Fruit", 
         {
-            timestamps: false,
+            name: {
+                type: Sequelize.STRING(20),
+                allowNull: false,
+                // get() { // 修改返回值
+                //     const fname = this.getDataValue("name");
+                //     const price = this.getDataValue("price");
+                //     const stock = this.getDataValue("stock");
+                //     return `${fname}(价格：￥${price} 库存：${stock}kg)`;
+                // }
+            },
+            price: {
+                type: Sequelize.FLOAT,
+                allowNull: false,
+                validate: { // 校验
+                    isFloat: { msg: "价格字段请输入数字" },
+                    min: { args: [0], msg: "价格字段必须大于0" }
+                }
+            },
+            stock: { type: Sequelize.INTEGER, defaultValue: 0 },
+        },
+        {
+            timestamps: false, // 时间戳：默认为true--存在
             getterMethods: {
                 amount() {
                     return this.getDataValue("stock") + "kg";
@@ -43,11 +43,13 @@
                 amount(val) {
                     const idx = val.indexOf('kg');
                     const v = val.slice(0, idx);
-                    this.setDataValue('stock', v);
+                    this.setDataValue('stock', v); // 放进数据库
                 }
             }
-        });
+        }
+    );
 
+    // 模型化方法
     Fruit.classify = function (name) {
         const tropicFruits = ['香蕉', '芒果', '椰子']; // 热带水果
         return tropicFruits.includes(name) ? '热带水果' : '其他水果';
@@ -59,13 +61,17 @@
     ['香蕉', '草莓'].forEach(f => console.log(f + '是' + Fruit.classify(f)));
 
     // 同步数据库，force: true则会删除已存在表
-    let ret = await Fruit.sync({ force: false })
+    let ret = await Fruit.sync({ force: true })
     // console.log('sync', ret)
+
+    // insert
     ret = await Fruit.create({
         name: "香蕉",
         price: 3.5
     })
     // console.log('create', ret)
+
+    // 查询
     // ret = await Fruit.findAll()
 
     // 使用实例方法
@@ -81,12 +87,12 @@
 
     // console.log('findAll', ret.amount, JSON.stringify(ret))
 
-    // Fruit.findAll().then(fruits => {
-    //     console.log(JSON.stringify(fruits));
-    //     // 修改amount，触发setterMethods
-    //     fruits[0].amount = '150kg';
-    //     fruits[0].save();
-    // });
+    Fruit.findAll().then(fruits => {
+        console.log(JSON.stringify(fruits));
+        // 修改amount，触发setterMethods
+        fruits[0].amount = '150kg';
+        fruits[0].save();
+    });
 
     // Fruit.findOne({ attributes: ['name'] }).then(fruit => {
     //     // fruit是首个匹配项，若没有则为null
